185d406a19556bcfe0e3bb0bf86c14667c1ca037
mun-jihye <m84422583@gmail.com>
2024-07-18 20:02:42 +0900

    App component 분리

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import routes from "./utils/constants/routes";
-import HomePage from "./page/HomePage";
-import TodoPage from "./page/TodoPage";
-
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path={routes.home} element={<HomePage />} />
-        <Route path={routes.todo} element={<TodoPage />} />
-      </Routes>
-    </Router>
-  );
-}
 
 root.render(<App />);
